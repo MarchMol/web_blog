50d612baae6d53a028897f461cfa46b229c92f55
MarchMol <marchmol98@gmail.com>
2024-04-28 14:49:17 -0600

    Fix: endpoints

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,20 +1,17 @@
 import conn from './conn.js'
 
 export async function getAllPosts() {
-
     let client;
     try {
         client = await conn.connect();
-        const result = await client.query('SELECT * FROM your_table');
+        const result = await client.query('SELECT * FROM movie_blog');
 
         console.log(result.rows);
         return rows
     } catch (error) {
-        // Handle any errors
-        console.error('Error executing query', error);
+        console.error('Error seleccionando registros', error);
 
     } finally {
-        // Release the client back to the pool
         if (client) {
             client.release();
         }
@@ -22,6 +19,20 @@ export async function getAllPosts() {
 }
 
 export async function createPost(title, content, image_url) {
-    const [result] = await conn.query(`INSERT INTO movie_blog (title,content,publish_date,image_url) VALUES ('${title}', '${content}', CURRENT_DATE, '${image_url}')`)
-    return result
+
+    let client;
+    try {
+        client = await conn.connect();
+        const result = await conn.query(`INSERT INTO movie_blog (title,content,publish_date,image_url) VALUES ('${title}', '${content}', CURRENT_DATE, '${image_url}')`)
+
+        console.log(result);
+        return result
+    } catch (error) {
+        console.error('Error insertando registros', error);
+
+    } finally {
+        if (client) {
+            client.release();
+        }
+    }
 }
