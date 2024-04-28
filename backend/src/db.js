fd48eaca3d9894a4698fbfe0d12da44ae85dd64d
MarchMol <marchmol98@gmail.com>
2024-04-28 14:42:12 -0600

    Fix: pg client

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,27 @@
 import conn from './conn.js'
 
 export async function getAllPosts() {
-    const [rows] = await conn.query('SELECT * FROM movie_blog')
-    return rows
+
+    let client;
+    try {
+        client = await conn.connect();
+        const result = await client.query('SELECT * FROM your_table');
+
+        console.log(result.rows);
+        return rows
+    } catch (error) {
+        // Handle any errors
+        console.error('Error executing query', error);
+
+    } finally {
+        // Release the client back to the pool
+        if (client) {
+            client.release();
+        }
+    }
 }
 
 export async function createPost(title, content, image_url) {
-    const [result] = await conn.query(`INSERT INTO blog_posts (title,content,publish_date,image_url) VALUES ('${title}', '${content}', CURRENT_DATE, '${image_url}')`)
+    const [result] = await conn.query(`INSERT INTO movie_blog (title,content,publish_date,image_url) VALUES ('${title}', '${content}', CURRENT_DATE, '${image_url}')`)
     return result
-  }
+}
