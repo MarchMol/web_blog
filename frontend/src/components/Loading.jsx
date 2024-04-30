552bcc1e38ace82bcb5a9898a4517273a95d0680
MarchMol <marchmol98@gmail.com>
2024-04-30 17:47:00 -0600

    Feat: loading component and logo

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 import './Loading.css'
 
-const Loading = () => {
-      return ( 
-        <center>
-<img src="https://icones.pro/wp-content/uploads/2021/06/icone-chargement-jaune.png" alt="Cargando Contenido" className="loading" />
-        </center>
-    );
+const Loading = ({ isLoading, children }) => {
+  return (
+    <center>
+      {isLoading ?
+        (
+          <>
+            {children}
+            <div className="disableLoading">
+              <img src="https://icones.pro/wp-content/uploads/2021/06/icone-chargement-jaune.png" alt="Cargando Contenido" className="loading" />
+            </div>
+            
+          </>)
+          :(
+            <>
+            {children}
+            </>
+            
+          )
+          }
+    </center>
+  );
 }
 
 export default Loading
---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import './Logo.css'
+import logo_amp from '../assets/logo_amp.png';
+
+function Logo() {
+
+    return(
+        <div className="logo">
+            <p><b>Beat <br /> Buzz</b></p>
+            <img src={logo_amp} alt="amp" />
+        </div>
+    )
+
+}
+
+export default Logo
