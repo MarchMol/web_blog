8ffb809e9ab69695f70ab828dbc2eb028e44d091
MarchMol <marchmol98@gmail.com>
2024-04-30 19:46:03 -0600

    create test10

--- backend/index.js
+++ backend/index.js
@@ -83,20 +83,20 @@ app.post('/create/', [
     return res.status(400).json({ errors: 'Formato incorrecto' })
   }
   const {
-    name, album,artist, music, cover_art, content, rank, album_date,
+    name, album, artist, music, cover_art, content, rank, album_date,
   } = req.body
 
   try {
-    // var token = req.headers['Authorization'].split(' ')[1];
-    // if(validateToken(token)){
-      const posts = await createPost(name, album,artist, music, cover_art, content, rank, album_date)
+    var token = req.headers['Authorization'].split(' ')[1];
+    if(validateToken(token)){
+      const posts = await createPost(name, album, artist, music, cover_art, content, rank, album_date)
       res.status(200)
       res.json(posts)
       return 
-    // }else{
-    //   res.status(401).json({ "success": false })
-    //   return 
-    // }
+    }else{
+      res.status(401).json({ "success": false })
+      return 
+    }
   } catch (error) {
     return res.status(500).json({ error: 'Ocurrio un error creando los posts' })
   }
