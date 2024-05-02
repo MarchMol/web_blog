955f2cac11f88363ce99505e3d90785882af740c
MarchMol <marchmol98@gmail.com>
2024-05-02 01:35:20 -0600

    Fix: logo margin and loading

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -17,7 +17,6 @@ const Loading = ({ isLoading, children }) => {
             <>
             {children}
             </>
-            
           )
           }
     </center>
--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -4,10 +4,14 @@ import logo_amp from '../assets/logo_amp.png';
 function Logo() {
 
     return(
+        <>
+        
         <div className="logo">
             <p><b>Beat <br /> Buzz</b></p>
             <img src={logo_amp} alt="amp" />
         </div>
+        </>
+
     )
 
 }
