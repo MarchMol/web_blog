bcdf3128c029cd1e3d0b17ddd777260768728e04
MarchMol <marchmol98@gmail.com>
2024-05-02 01:33:58 -0600

    Feat: icons

---
+++ frontend/src/components/Icon.jsx
@@ -0,0 +1,18 @@
+import './Icon.css'
+import PropTypes from 'prop-types'
+
+function Icon ({type, onClick, enabled}) {
+    return (
+        <div className={enabled ? ('iconContainer'): 'disabledIcon'} onClick={onClick}>
+            <div className={type}></div>
+        </div>
+    )
+}
+
+Icon.propTypes = {
+    type: PropTypes.string.isRequired,
+    enabled: PropTypes.bool,
+    onClick: PropTypes.func
+}
+
+export default Icon
