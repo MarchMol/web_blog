8e586ac597328c5aa3694c19dd8b95d71eed1b1b
MarchMol <marchmol98@gmail.com>
2024-05-08 02:12:45 -0600

    Feat: secret now secure

--- backend/jwt.js
+++ backend/jwt.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
-const SECRET = 'purple-yellow-red-&-blue'
+const SECRET = process.env.SECRET
 
 const generateToken = (user) => {
   return jwt.sign(user, SECRET, { expiresIn: '15min', algorithm: 'HS256' })
