99c140cf2be8b1dfc32cd99b69618932ee21c0b0
MarchMol <marchmol98@gmail.com>
2024-05-07 21:22:30 -0600

    Fix: login now uses form hook

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 import { md5 } from 'js-md5'
+import { object, string, number, date } from 'yup';
 import './Login.css'
 import Input from '@components/Input.jsx'
 import Button from '@components/Button.jsx'
@@ -9,18 +10,23 @@ import UseToken from '@hooks/UseToken.jsx'
 import useApi from '@hooks/useApi.jsx'
 import Message from '@components/Message'
 import useMsg from '@hooks/useMsg'
+import useForm from '@hooks/useForm';
 
+let loginSchema = object({
+    username: string().max(255).required(),
+    password: string().max(255).required()
+});
 
 function Login({ onSignal }) {
-    const [formState, setFormState] = useState({ username: '', password: '' })
+    const { values, setValue } = useForm(loginSchema)
     const { setToken } = UseToken();
     const { loading, fetchData} = useApi()
     const { setIsModalOpen, setMsg, setIsChoice } = useMsg()
 
     const handleSubmit = async () => {
         const body = {}
-        body.username = formState.username
-        body.password = md5(formState.password)
+        body.username = values.username
+        body.password = md5(values.password)
         const rslt = await fetchData('login', 'https://web-blog-inky.vercel.app/login', body)
         if (rslt) {
             setToken(rslt.access_token)
@@ -32,21 +38,15 @@ function Login({ onSignal }) {
         }
     }
 
-    const setValue = (name, value) => {
-        setFormState({
-            ...formState,
-            [name]: value
-        });
-    };
 
     return (
         <Loading isLoading={loading}>
                 <div className="container">
                     <h1>Login</h1>
                     <form>
-                        <Input label='Username' type='text' value={formState.username}
+                        <Input label='Username' type='text' value={values.username}
                             onChange={(value) => setValue('username', value)} size='normalInput' max={255} />
-                        <Input label='Password' type='password' value={formState.password}
+                        <Input label='Password' type='password' value={values.password}
                             onChange={(value) => setValue('password', value)} size='normalInput' max={255} />
                         <Button text="Login" onClick={handleSubmit} />
                     </form>
